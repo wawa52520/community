461035babe5ed5db3708968259019aa8c977eb9b
wawa52520 <877048038@qq.com>
2020-02-21 20:58:18 +0800

    community

---
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -0,0 +1,15 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "key")String value, Model model){
+        model.addAttribute("html_Name",value);
+        return "hello";
+    }
+}
